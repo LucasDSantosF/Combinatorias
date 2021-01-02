932de81ce83ae0b93e942a0620cd2197d9427d3e
Lucas Dos Santos Francisco <lucassantosf@outlook.com.br>
2021-01-02 19:09:29 -0300

    Parte final

--- Combinatoria/__init__.py
+++ Combinatoria/__init__.py
@@ -1,7 +1,10 @@
 def Permuta(num):
-    soma = num
-    for i in range(1, num):
-        soma = soma*i
+    if num != 0:
+        soma = num
+        for i in range(1, num):
+            soma = soma*i
+    else:
+        soma = 1 
     
     return soma
 
@@ -20,3 +23,80 @@ def CombinatoriaSimples(n, r):
     resposta = dividendo / Permuta(divisor)
 
     return resposta
+
+
+def Comb_Complementar(n, p):
+    r = n - p
+
+    return CombinatoriaSimples(n, r)
+
+
+def Kaplansky(n, p):
+    num = n-p+1
+
+    return CombinatoriaSimples(num, p)
+
+
+def InteirosPositivos(m, n):
+    m = m-1
+    n = n-1
+
+    return CombinatoriaSimples(m, n)
+
+
+def Nao_Negativos(m, n):
+    m = m+n-1
+    n = n-1
+
+    return CombinatoriaSimples(m, n)
+
+
+def Comb_Repeticao(n, p):
+    r = n-1
+    n = n+p-1
+
+    return CombinatoriaSimples(n, r)
+
+
+def Permuta_Repeticao(n, lista):
+    soma = 1
+    for i in range(0, len(lista)):
+        r = lista[i]
+        lista[i] = CombinatoriaSimples(n, r)
+        n -= r
+        soma *= lista[i]
+    
+    return soma
+
+
+def Arranjo_Repeticao(m, p):
+
+    return m**p
+
+
+def Permuta_Circular(n):
+    n = n-1
+
+    return Permuta(n)
+
+
+def Permuta_Caotica(n):
+
+    lista = []
+
+    for i in range(0, n+1):
+        lista.append(Permuta(i))
+    
+    aux = 1
+    soma = 0
+    num = Permuta(n)
+    for i in range(0, len(lista)):
+        if aux%2 == 1:
+            soma += num/lista[i]
+        else:
+            soma -= num/lista[i]
+        
+        aux += 1
+    
+    return soma
+        
--- teste.py
+++ teste.py
@@ -7,3 +7,21 @@ num = 5
 # print(Arranjo(num, 1))
 
 # print(CombinatoriaSimples(7, 4))
+
+# print(Comb_Complementar(7, 5))
+
+# print(Kaplansky(5, 2))
+
+# print(InteirosPositivos(4, 2))
+
+# print(Nao_Negativos(4, 2))
+
+# print(Comb_Repeticao(2,4))
+
+# print(Permuta_Repeticao(7, [3,2,2]))
+
+# print(Arranjo_Repeticao(26,2))
+
+# print(Permuta_Circular(5))
+
+# print(Permuta_Caotica(5))
